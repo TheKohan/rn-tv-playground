efc02eeba5e780c797040b4b655cd38522ea8671
TheKohan <kohan142011@gmail.com>
2025-11-24 13:20:34 +0100

    changes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, TouchableOpacity, useTVEventHandler, View } from 'react-native';
-import React from 'react';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TVFocusGuideView,
+  useTVEventHandler,
+  View,
+} from "react-native";
+import React, { memo, useMemo, useRef } from "react";
 
 export default function App() {
-  const [lastEventType, setLastEventType] = React.useState('');
+  const [lastEventType, setLastEventType] = React.useState("");
+  const [prevClicked, setPrevClicked] = React.useState("");
 
-  const myTVEventHandler = evt => {
+  const myTVEventHandler = (evt) => {
     setLastEventType(evt.eventType);
   };
 
@@ -12,29 +21,168 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => {}}>
-        <Text style={styles.text}>
-          This example enables an instance of TVEventHandler to show the last
-          event detected from the Apple TV Siri remote or from a keyboard.
-        </Text>
-      </TouchableOpacity>
-      <Text style={{color: 'blue'}}>{lastEventType}</Text>
+      <TVFocusGuideView autoFocus>
+        <View style={{ flexDirection: "row", gap: 10 }}>
+          <Nav />
+          <View>
+            <Hero />
+            <Grid />
+          </View>
+        </View>
+      </TVFocusGuideView>
     </View>
   );
-
-
 }
+
+const Hero = () => {
+  return (
+    <TVFocusGuideView autoFocus>
+      <View style={styles.hero}>
+        <Text style={styles.text}>This is hero Image</Text>
+        <TouchableOpacity style={styles.playButton}>
+          <Text style={styles.text}>Play Now</Text>
+        </TouchableOpacity>
+      </View>
+    </TVFocusGuideView>
+  );
+};
+
+const GridItem = memo(({ number }: { number: number }) => {
+  const backgroundColor = useMemo(() => getRandomColor(), []);
+
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor,
+        padding: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 10,
+        margin: 10,
+        width: 100,
+      }}
+    >
+      <Text style={styles.text}>{number}</Text>
+    </TouchableOpacity>
+  );
+});
+
+const Grid = () => {
+  return (
+    <View>
+      <TVFocusGuideView trapFocusLeft>
+        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+          <GridItem number={1} />
+          <GridItem number={2} />
+          <GridItem number={3} />
+          <GridItem number={4} />
+          <GridItem number={5} />
+          <GridItem number={6} />
+          <GridItem number={7} />
+          <GridItem number={8} />
+          <GridItem number={9} />
+          <GridItem number={10} />
+          <GridItem number={11} />
+          <GridItem number={12} />
+          <GridItem number={13} />
+          <GridItem number={14} />
+          <GridItem number={15} />
+          <GridItem number={16} />
+          <GridItem number={17} />
+          <GridItem number={18} />
+          <GridItem number={19} />
+          <GridItem number={20} />
+        </View>
+      </TVFocusGuideView>
+
+      <View>
+        <TouchableOpacity>
+          <Text>Outside Button</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const NavItem = ({
+  children,
+  ref,
+}: {
+  children: React.ReactNode;
+  ref?: React.RefObject<View>;
+}) => {
+  return (
+    <TouchableOpacity style={styles.navItem} ref={ref}>
+      {children}
+    </TouchableOpacity>
+  );
+};
+
+const Nav = () => {
+  const about = useRef<View>(null);
+
+  return (
+    <TVFocusGuideView trapFocusRight destinations={[about.current]}>
+      <View style={styles.nav}>
+        <NavItem>
+          <Text style={styles.text}>Home</Text>
+        </NavItem>
+        <NavItem ref={about}>
+          <Text style={styles.text}>About</Text>
+        </NavItem>
+        <NavItem>
+          <Text style={styles.text}>Live</Text>
+        </NavItem>
+        <NavItem>
+          <Text style={styles.text}>Settings</Text>
+        </NavItem>
+      </View>
+    </TVFocusGuideView>
+  );
+};
+
+const getRandomColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingHorizontal: 24,
+  },
+  hero: {
+    padding: 20,
+    backgroundColor: "red",
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  playButton: {
+    padding: 10,
+    backgroundColor: "blue",
+    borderRadius: 10,
   },
   text: {
-    color: '#000',
     fontSize: 20,
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  nav: {
+    padding: 20,
+    backgroundColor: "blue",
+    borderRadius: 10,
+    height: "100%",
+    gap: 10,
+  },
+  navItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 10,
+    padding: 10,
+    alignItems: "center",
+    backgroundColor: "magenta",
   },
 });
