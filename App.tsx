c22a3410a2a5918f76aec2054c1cc351655ac836
TheKohan <kohan142011@gmail.com>
2025-11-24 12:17:05 +0100

    wip

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, useTVEventHandler, View } from 'react-native';
 import React from 'react';
 
 export default function App() {
+  const [lastEventType, setLastEventType] = React.useState('');
+
+  const myTVEventHandler = evt => {
+    setLastEventType(evt.eventType);
+  };
+
+  useTVEventHandler(myTVEventHandler);
+
   return (
     <View style={styles.container}>
-      <Text>Test App!!</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style={styles.text}>
+          This example enables an instance of TVEventHandler to show the last
+          event detected from the Apple TV Siri remote or from a keyboard.
+        </Text>
+      </TouchableOpacity>
+      <Text style={{color: 'blue'}}>{lastEventType}</Text>
     </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +31,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
